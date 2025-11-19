6b20913025a166c9dd2d0c66d2e8be79ee3c5fe8
Victorlng <victor.lelong57@orange.fr>
2025-11-19 17:20:12 +0100

    Auto-commit: 2 modified files

--- Essais/essaiaffichage.c
+++ Essais/essaiaffichage.c
@@ -70,10 +70,11 @@ int main(){
         printf("Première table :\n");
         printf("   1  2  3  4  5  6  7\n");
         for(i=0,c=8;i<=20;i++,c++){
-            printf("%c  ",tab4[i]);
+            
             if(c>7){
             printf("\n   ");
             c=1;
+            printf("%c  ",tab4[i]);
         }
         }
         break;
