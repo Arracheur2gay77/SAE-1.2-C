6b93f7389057426decc7ec1182777e6dc21d4961
Victorlng <victor.lelong57@orange.fr>
2025-11-19 17:12:09 +0100

    Auto-commit: 1 modified file

--- Essais/essai.c
+++ Essais/essai.c
@@ -1,6 +1,55 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include <string.h>
 
 int main(){
+    srand((unsigned int)time(NULL));
+    int tab1[20],tab2[20],tab3[20],i=0,j,n,exist;
+
+    while(1){
+        tab1[i]=i;
+        printf("%d ",tab1[i]);
+        i+=1;
+
+        if(i>20) break;
+    }
+
+    printf("\n");
+
+    for(i=0;i<21;i++){
+        do{
+            n=rand()%21+0;
+            exist=0;
+
+            for(j=0;j<i;j++){
+                if(tab2[j]==n){exist=1;break;}
+            }
+
+        }while(exist==1);
+
+        tab2[i]=n;
+        printf("%d ",n);
+    }
+
+    printf("\n");
+
+    for(i=0;i<21;i++){
+        do{
+            n=rand()%21+0;
+            exist=0;
+
+            for(j=0;j<i;j++){
+                if(tab3[j]==n){exist=1;break;}
+            }
+
+        }while(exist==1);
+
+        tab3[i]=n;
+        printf("%d ",n);
+    }
+    printf("\n\n\n");
+
     int i,c;
     char tab4[21];
 
