56b7d3eb54241bb59f681c6f4468a6a27556427c
Victorlng <victor.lelong57@orange.fr>
2025-11-19 16:52:47 +0100

    Auto-commit: 1 deleted file

--- temp.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-
-printf("   1  2  3  4  5  6  7\n");
-            printf("1 ## ## ## ## ## ## ##\n");
-            printf("2 ## ## ## ## ## ## ##\n");
-            printf("3 ## ## ## ## ## ## ##\n");
-            }
