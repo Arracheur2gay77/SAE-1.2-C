3cb6cee2efb815a197e3d74700a3b093e6aebff4
Victorlng <victor.lelong57@orange.fr>
2025-11-19 17:07:31 +0100

    Auto-commit: 2 modified files

--- essai.c
+++ essai.c
@@ -5,8 +5,8 @@ int main(){
     char tab4[21][2];
 
     for(i=0,c=1;i<=20;i++,c++){
+        tab4[i][0]='#';
         tab4[i][1]='#';
-        tab4[i][2]='#';
         if(c>7){
             printf("\n");
             c=1;
