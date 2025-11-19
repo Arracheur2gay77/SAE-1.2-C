5487cdf8a4a0aa7fea24c00a5601c07d94a43b62
Victorlng <victor.lelong57@orange.fr>
2025-11-19 15:57:29 +0100

    Auto-commit: 1 modified file

---
+++ maquetteprog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main (){
+    int tab1[20],i=0;
+
+    while(1){
+        tab1[i]=i;
+        printf("%d ",tab1[i]);
+        i+=1;
+
+        if(i>20) break;
+    }
+
+    int n;
+
+    while(1){
+        printf("Veuillez entrer")
+    }
+}
