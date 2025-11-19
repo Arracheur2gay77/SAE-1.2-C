043c8afd2cd7e399fe8fd14707a9c010612d4ed1
Victorlng <victor.lelong57@orange.fr>
2025-11-19 16:52:21 +0100

    Auto-commit: 1 added, 1 modified files

---
+++ temp.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+
+printf("   1  2  3  4  5  6  7\n");
+            printf("1 ## ## ## ## ## ## ##\n");
+            printf("2 ## ## ## ## ## ## ##\n");
+            printf("3 ## ## ## ## ## ## ##\n");
+            }
