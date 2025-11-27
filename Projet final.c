4606f3d48c1f036285677ecd3684db1432faa060
Victorlng <victor.lelong57@orange.fr>
2025-11-27 21:24:29 +0100

    Auto-commit: 2 modified files

--- Projet final.c
+++ Projet final.c
@@ -91,8 +91,8 @@ int main(){
             printf("Erreur : entrez un nombre entre 1 et 21 !\n");
         }
 
-        tab4[nb1-1]=(tab2[nb1-1]);
-        tab5[nb2-1]=(tab3[nb2-1]);
+        tab4[nb1-1]=tab2[nb1-1];
+        tab5[nb2-1]=tab3[nb2-1];
 
         printf("\n=== Mise à jour des tableaux ===\n\n");
 
